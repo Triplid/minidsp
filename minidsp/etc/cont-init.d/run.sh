705471052a7aa0c8a0a8aadd87894808d4626509
Triplid <58692279+Triplid@users.noreply.github.com>
2025-11-19 16:40:09 +0300

    Update run.sh

--- minidsp/etc/cont-init.d/run.sh
+++ minidsp/etc/cont-init.d/run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/with-contenv bash
-# cont-init.d автоматически исполняется s6-overlay
-echo "miniDSP API starting..."
-# gunicorn запускаем через venv
+echo "Starting miniDSP API..."
+# gunicorn или python скрипт из venv
 /opt/venv/bin/gunicorn --bind 0.0.0.0:8080 api:app
