4eaca338193311af555a0ee4ad652b562ac60be2
Triplid <58692279+Triplid@users.noreply.github.com>
2025-11-19 16:47:12 +0300

    Update run.sh

--- minidsp/etc/cont-init.d/run.sh
+++ minidsp/etc/cont-init.d/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/with-contenv bash
-echo "Starting miniDSP API..."
-# gunicorn или python скрипт из venv
+# Этот скрипт запускается автоматически через s6-overlay при старте контейнера
+
+echo "Starting miniDSP Python API..."
+# Запускаем gunicorn из venv без exec, чтобы PID=1 оставался за s6
 /opt/venv/bin/gunicorn --bind 0.0.0.0:8080 api:app
